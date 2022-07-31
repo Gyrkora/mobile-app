30056346b077d96fce8b5a7afda38abbb6dde14b
Gyrkora <giselle.gallardo0210@gmail.com>
2022-07-31 01:34:26 -0300

    casi listo

--- my-app/components/MathsGame/AlternativesContainer.js
+++ my-app/components/MathsGame/AlternativesContainer.js
@@ -1,12 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
+import colors from '../../constants/colors'
 
 const AlternativesContainer = (props) => {
+	const prueba = () => {
+		console.log('funciona sin button')
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text style={{ color: 'red' }}>{props.suma}</Text>
+			<View
+				style={styles.altContainer}
+				onPress={() => {
+					prueba()
+				}}
+			>
+				<Text style={{ ...styles.alternatives, color: 'red' }}>
+					{props.suma}
+				</Text>
+			</View>
+			<View style={styles.altContainer}>
+				<Text style={styles.alternatives}>{props.random}</Text>
+			</View>
 
-			<Text>{props.random}</Text>
-			<Text>{props.random}</Text>
+			<View style={styles.altContainer}>
+				<Text style={styles.alternatives}>{props.random}</Text>
+			</View>
 		</View>
 	)
 }
@@ -15,6 +33,24 @@ const styles = StyleSheet.create({
 	container: {
 		flexDirection: 'row',
 		justifyContent: 'space-evenly',
+		alignItems: 'center',
+	},
+
+	altContainer: {
+		backgroundColor: colors.blue,
+		textAlign: 'center',
+		padding: 10,
+		borderRadius: 10,
+		marginTop: 30,
+	},
+
+	alternatives: {
+		fontSize: 20,
+		color: colors.beige,
+		width: 80,
+		paddingTop: 10,
+		paddingBottom: 10,
+		textAlign: 'center',
 		alignItems: 'center',
 	},
 })
