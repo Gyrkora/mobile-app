b265a8dc80c7f8dc3d60513de67d0cc1beeacbdc
Gyrkora <giselle.gallardo0210@gmail.com>
2022-08-30 20:58:20 -0400

    APP State

--- my-app/store/reducers/shop.reducer.js
+++ my-app/store/reducers/shop.reducer.js
@@ -6,17 +6,7 @@ const initialState = {
 }
 
 const ProductReducer = (state = initialState, action) => {
-	// switch (action.type) {
-	// 	case SELECT_PRODUCT:
-	// 		const IndexCategory = state.products.findIndex(
-	// 			(cat) => cat.id === action.categoryID
-	// 		)
-
-	// 		if (IndexCategory === -1) return state
-	// 		return { ...state, selected: state.products[IndexCategory] }
-	// 	default:
 	return state
-	// }
 }
 
 export default ProductReducer
